878d2f1abf692cfeb4a67a216c4e3a1676946406
Daniel Ochoa <daochoam>
2025-03-23 12:35:22 -0500

    update setting linter prettier vuejs

---
+++ frontend/vuejs/linters/eslinter-prettier/README.md
@@ -0,0 +1,50 @@
+# Vue.js Project
+
+This is a Vue.js project.
+
+## Installation
+
+Run the following command to install ESLint and related dependencies:
+
+```bash
+npm install globals eslint globals prettier eslint-config-prettier eslint-plugin-prettier eslint-plugin-vue @babel/core @babel/eslint-parser @babel/preset-env @eslint/js @vue/cli-plugin-babel --save-dev
+```
+
+## Scripts
+
+Add the following lines to the `scripts` section in your `package.json`:
+
+```json
+"scripts": {
+  "format": "prettier --write '**/*.{js,vue,json,css,md}'",
+  "lint": "eslint '**/*.{js,vue,json,css,md}'",
+  "lint:fix": "eslint '**/*.{js,vue,json,css,md}' --fix --ignore-pattern .gitignore"
+}
+```
+
+## Prettier Configuration
+
+Create a `.prettierrc` file in the root of your project and add the following content:
+
+```json
+{
+  "printWidth": 100,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "htmlWhitespaceSensitivity": "ignore",
+  "vueIndentScriptAndStyle": true
+}
+```
+
+## Usage
+
+- `npm run lint`: Lint files.
+- `npm run lint:fix`: Lint and fix files.
+- `npm run format`: Format code using Prettier.
+
+```
+
+```
---
+++ frontend/vuejs/linters/eslinter-prettier/eslint.config--module.js
@@ -0,0 +1,144 @@
+import babelParser from '@babel/eslint-parser'
+import pkg from '@eslint/js'
+import prettierPlugin from 'eslint-plugin-prettier'
+import vuePlugin from 'eslint-plugin-vue'
+import globals from 'globals'
+import vueParser from 'vue-eslint-parser'
+
+const { configs: jsConfigs } = pkg
+
+export default [
+  {
+    files: ['src/**/*.{js,mjs,cjs}'],
+    ignores: [
+      'node_modules/',
+      'public/',
+      'package-lock.json',
+      'package.json',
+      '*.config.js',
+      '*.config.cjs',
+      '*.config.mjs'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        requireConfigFile: false
+      }
+    },
+    rules: {
+      ...jsConfigs.recommended.rules,
+      'no-unused-vars': 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+    }
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        parser: babelParser,
+        requireConfigFile: false
+      }
+    },
+    plugins: {
+      vue: vuePlugin,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...vuePlugin.configs['essential'].rules,
+      ...prettierPlugin.configs.recommended.rules,
+      'no-inline-comments': 'error',
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: 4, // Max 4 attribute per line for single-line elements
+          multiline: {
+            max: 1 // Max 1 attribute per line for multi-line elements
+          }
+        }
+      ],
+      // Enforce a specific order for attributes in Vue components
+      'vue/attributes-order': [
+        'error',
+        {
+          order: [
+            'GLOBAL', // id
+            'DEFINITION', // is
+            'LIST_RENDERING', // v-for
+            'CONDITIONALS', // v-if, v-else-if, v-else, v-show, v-cloak
+            ['UNIQUE', 'SLOT'], // ref, key, v-slot
+            'TWO_WAY_BINDING', // v-model
+            'OTHER_ATTR', // props, class, style
+            'OTHER_DIRECTIVES', // Other directives not covered above
+            'RENDER_MODIFIERS', // v-once, v-pre, v-memo
+            'CONTENT', // v-text, v-html
+            'EVENTS' // v-on or @
+          ],
+          alphabetical: false // Optional: disable alphabetical sorting within groups
+        }
+      ],
+      'vue/order-in-components': [
+        'error',
+        {
+          order: [
+            'el',
+            'name',
+            'key',
+            'parent',
+            'functional',
+            ['delimiters', 'comments'],
+            ['components', 'directives', 'filters'],
+            'extends',
+            'mixins',
+            ['provide', 'inject'],
+            'ROUTER_GUARDS',
+            'layout',
+            'middleware',
+            'validate',
+            'scrollToTop',
+            'transition',
+            'loading',
+            'inheritAttrs',
+            'model',
+            ['props', 'propsData'],
+            'emits',
+            'slots',
+            'expose',
+            'setup',
+            'asyncData',
+            'data',
+            'fetch',
+            'head',
+            'computed',
+            'watch',
+            'watchQuery',
+            'LIFECYCLE_HOOKS',
+            'methods',
+            ['template', 'render'],
+            'renderError'
+          ]
+        }
+      ],
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true
+        }
+      ]
+    }
+  }
+]
