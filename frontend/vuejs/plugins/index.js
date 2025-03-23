84c1aa82541ff130a1e31b192b7cc36e1ccfe575
daochoam <dfom89@gmail.com>
2025-03-23 14:05:15 -0500

    Agregar el plugin useFetch para mejorar la gestión de API en Vue.js

--- frontend/vuejs/plugins/index.js
+++ frontend/vuejs/plugins/index.js
@@ -1,3 +1,5 @@
 export { default as axiosPromises } from './axiosPromises'
 export { default as globalComponent } from './globalComponent'
+export { default as useFetch } from './useFetch'
 export { default as vuetify } from './vuetify'
+
